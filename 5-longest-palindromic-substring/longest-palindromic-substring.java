36a1eb2edf36c85513deb4582c4fdc4da2189d79
Gomare Pragati Chandrakant <109347536+Pragatitech14@users.noreply.github.com>
2026-07-02 18:02:07 +0530

    Time: 13 ms (96.27%) | Memory: 43.5 MB (79.86%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,38 @@
 class Solution {
-
-    int start = 0;
-    int maxLen = 1;
+    int start =0;
+    int maxlen =1;
 
     public String longestPalindrome(String s) {
+       
+        if(s==null || s.length()==0) return "";
 
-        if (s == null || s.length() == 0)
-            return "";
 
-        for (int i = 1; i < s.length(); i++) {
+        for(int i =1;i<s.length();i++)
+        {
+            //even length
+            expand(s,i-1,i);
+            //odd length
+            expand(s,i-1,i+1);
+        } 
 
-            // Even length palindrome
-            expand(s, i - 1, i);
+      return s.substring(start,start+maxlen);  
+  }
 
-            // Odd length palindrome
-            expand(s, i - 1, i + 1);
-        }
-
-        return s.substring(start, start + maxLen);
+  public void expand(String s,int left,int right)
+  {
+    
+    while(left>=0 && right<s.length() 
+    && s.charAt(left)==s.charAt(right))
+    {
+       
+       if(right-left+1>maxlen)
+       {
+        maxlen = right-left+1;
+        start=left;
+       }
+       left--;
+       right++;
     }
-
-    private void expand(String s, int left, int right) {
-
-        while (left >= 0 && right < s.length()
-                && s.charAt(left) == s.charAt(right)) {
-
-            if (right - left + 1 > maxLen) {
-                maxLen = right - left + 1;
-                start = left;
-            }
-
-            left--;
-            right++;
-        }
-    }
+    
+  }
 }
