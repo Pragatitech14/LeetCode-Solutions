cb9b22d374b9e30628831ea1a1cb22694d8d8689
Gomare Pragati Chandrakant <109347536+Pragatitech14@users.noreply.github.com>
2026-07-07 22:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (82.51%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1; 
+            }
+        }
+
+        
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
+        System.out.println("Output: " + sol.searchInsert(nums1, target1)); 
+
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+        System.out.println("Output: " + sol.searchInsert(nums2, target2)); 
+
+        int[] nums3 = {1, 3, 5, 6};
+        int target3 = 7;
+        System.out.println("Output: " + sol.searchInsert(nums3, target3)); 
+    }
+}
