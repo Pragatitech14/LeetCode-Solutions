89cbf44bc1d3b99a8040d79ac6d7ad95265f37d7
Gomare Pragati Chandrakant <109347536+Pragatitech14@users.noreply.github.com>
2026-08-18 01:02:11 +0530

    Time: 1 ms (96.23%) | Memory: 45 MB (90.26%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -3,27 +3,24 @@ class Solution {
 
         // Step 1: Apply operations
         for (int i = 0; i < nums.length - 1; i++) {
-
             if (nums[i] == nums[i + 1]) {
-                nums[i] = nums[i] * 2;
+                nums[i] *= 2;
                 nums[i + 1] = 0;
             }
         }
 
-        // Step 2: Move non-zero elements to the front
+        // Step 2: Move all non-zero elements to front
         int index = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                index++;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
             }
         }
 
         // Fill remaining positions with 0
         while (index < nums.length) {
-            nums[index] = 0;
-            index++;
+            nums[index++] = 0;
         }
 
         return nums;
